7d460762d6acbba26256f68a3a7f35d0fbb4a4dc
Alex Browne <stephenalexbrowne@gmail.com>
2015-03-24 16:56:23 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"encoding/hex"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	srvAddr         = "224.0.0.1:9999"
+	maxDatagramSize = 8192
+)
+
+type message struct {
+	src  *net.UDPAddr
+	body []byte
+	n    int
+}
+
+func main() {
+	// go ping(srvAddr)
+	addr, err := net.ResolveUDPAddr("udp", srvAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgs := make(chan message, 100)
+	go receiveMessages(addr, msgs)
+	go printMessages(msgs)
+	go readInput(addr)
+	done := make(chan bool)
+	<-done
+}
+
+func readInput(addr *net.UDPAddr) {
+	reader := bufio.NewReader(os.Stdin)
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		msg, err := reader.ReadBytes('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := conn.Write(msg); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func receiveMessages(addr *net.UDPAddr, msgs chan message) {
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := conn.SetReadBuffer(maxDatagramSize); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		buf := make([]byte, maxDatagramSize)
+		n, src, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatal("ReadFromUDP failed:", err)
+		}
+		if n > 0 {
+			log.Printf("Read %d bytes from %s", n, src)
+			msg := message{
+				src:  src,
+				n:    n,
+				body: buf,
+			}
+			msgs <- msg
+		}
+	}
+}
+
+func printMessages(msgs chan message) {
+	for msg := range msgs {
+		log.Print(hex.Dump(msg.body[:msg.n]))
+	}
+}
